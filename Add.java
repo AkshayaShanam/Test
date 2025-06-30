6972267ceec432ac03dc0bbf1751677599fdac42
AkshayaShanam <akshayashanam0123@gmail.com>
2025-06-30 21:07:45 +0530

    Update Add.java

--- Add.java
+++ Add.java
@@ -1,8 +1,8 @@
-public class Add(){
+public class AddNumbers(){
    public static void main(String[] args){
       int a=4;
-     int b=4;
-     int c=a+b;
-     System.out.println(c);
+      int b=4;
+      int c=a+b;
+      System.out.println(c);
    }
 }
